c15112744daf8d4be6661b898c72eb4bb5be00d3
elsie <elimohl@gmail.com>
2015-01-27 22:18:16 +0300

    'hello world' with flask-bootstrap

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
