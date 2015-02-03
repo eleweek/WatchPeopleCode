44f789a82e91652463b1f88454479d56879b1f39
elsie <elimohl@gmail.com>
2015-02-03 21:14:50 +0300

    replace function for updating status to update_state.py

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ from wtforms.validators import ValidationError
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-import praw
 import os
-from utils import youtube_video_id, twitch_channel, requests_get_with_retries
-from bs4 import BeautifulSoup
+from utils import requests_get_with_retries
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
@@ -28,7 +26,6 @@ def run():
     app.run(debug=True)
 
 
-reddit_user_agent = "/r/WatchPeopleCode app"
 youtube_api_key = os.environ['ytokkey']
 
 
@@ -135,57 +132,6 @@ class TwitchStream(Stream):
     __mapper_args__ = {
         'polymorphic_identity': 'twitch_stream'
     }
-
-
-def get_new_stream_from_url(url):
-    ytid = youtube_video_id(url)
-    if ytid is not None:
-        if YoutubeStream.query.filter_by(ytid=ytid).first() is None:
-            return YoutubeStream(ytid)
-        else:
-            return None
-
-    tc = twitch_channel(url)
-    if tc is not None:
-        if TwitchStream.query.filter_by(channel=tc).first() is None:
-            return TwitchStream(tc)
-        else:
-            return None
-
-    return None
-
-
-def extract_links_from_selftexts(selftext_html):
-    soup = BeautifulSoup(selftext_html)
-    return [a['href'] for a in soup.findAll('a')]
-
-
-def get_new_streams():
-    r = praw.Reddit(user_agent=reddit_user_agent)
-    r.config.decode_html_entities = True
-
-    submissions = r.get_subreddit('watchpeoplecode').get_new(limit=50)
-    new_streams = set()
-    # TODO : don't forget about http vs https
-    # TODO better way of caching api requests
-    for s in submissions:
-        selfposts_urls = extract_links_from_selftexts(s.selftext_html) if s.selftext_html else []
-        for url in selfposts_urls + [s.url]:
-            # FIXME super ugly workaround :(
-            for i in xrange(10):
-                try:
-                    stream = get_new_stream_from_url(url)
-                    break
-                except:
-                    if i == 9:
-                        raise
-
-            if stream:
-                stream._get_api_status()
-                db.session.add(stream)
-                new_streams.add(stream)
-
-    db.session.commit()
 
 
 class CaseInsensitiveComparator(ColumnProperty.Comparator):
--- update_state.py
+++ update_state.py
@@ -1,6 +1,65 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from app import db, Stream, get_new_streams
+import praw
+from bs4 import BeautifulSoup
 from sqlalchemy import or_
+
+from app import db, Stream, YoutubeStream, TwitchStream
+from utils import youtube_video_id, twitch_channel
+
+
+def get_new_stream_from_url(url):
+    ytid = youtube_video_id(url)
+    if ytid is not None:
+        if YoutubeStream.query.filter_by(ytid=ytid).first() is None:
+            return YoutubeStream(ytid)
+        else:
+            return None
+
+    tc = twitch_channel(url)
+    if tc is not None:
+        if TwitchStream.query.filter_by(channel=tc).first() is None:
+            return TwitchStream(tc)
+        else:
+            return None
+
+    return None
+
+
+def extract_links_from_selftexts(selftext_html):
+    soup = BeautifulSoup(selftext_html)
+    return [a['href'] for a in soup.findAll('a')]
+
+
+reddit_user_agent = "/r/WatchPeopleCode app"
+
+
+def get_new_streams():
+    r = praw.Reddit(user_agent=reddit_user_agent)
+    r.config.decode_html_entities = True
+
+    submissions = r.get_subreddit('watchpeoplecode').get_new(limit=50)
+    new_streams = set()
+    # TODO : don't forget about http vs https
+    # TODO better way of caching api requests
+    for s in submissions:
+        selfposts_urls = extract_links_from_selftexts(s.selftext_html) if s.selftext_html else []
+        for url in selfposts_urls + [s.url]:
+            # FIXME super ugly workaround :(
+            for i in xrange(10):
+                try:
+                    stream = get_new_stream_from_url(url)
+                    break
+                except:
+                    if i == 9:
+                        raise
+
+            if stream:
+                stream._get_api_status()
+                db.session.add(stream)
+                new_streams.add(stream)
+
+    db.session.commit()
+
 
 sched = BlockingScheduler()
 
