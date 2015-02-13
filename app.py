4b3950764ddfabee2fa4b6f061b47056dab2f994
elsie <elimohl@gmail.com>
2015-02-13 19:24:39 +0300

    add __repr__ for submissions

--- app.py
+++ app.py
@@ -58,6 +58,9 @@ stream_sub = db.Table('stream_sub',
 class Submission(db.Model):
     submission_id = db.Column(db.String(6), primary_key=True)
 
+    def __repr__(self):
+        return '<Submission %r>' % (self.submission_id)
+
 
 class Stream(db.Model):
     id = db.Column(db.Integer, primary_key=True)
