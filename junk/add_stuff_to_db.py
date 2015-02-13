321f1f329e52e1ed63f215fa44f425eae6729977
elsie <elimohl@gmail.com>
2015-02-14 01:35:11 +0300

    one-time code for move subms to right table and delete twitch copies

--- junk/add_stuff_to_db.py
+++ junk/add_stuff_to_db.py
@@ -1,5 +1,5 @@
 from update_state import r, get_submission_urls, get_stream_from_url, get_reddit_username, get_or_create
-from app import Streamer, Submission, db
+from app import Streamer, Submission, TwitchStream, db
 
 
 def add_streamers():
@@ -23,6 +23,28 @@ def add_submissions():
         for url in get_submission_urls(s):
             stream = get_stream_from_url(url, s.id)
             if stream:
-                stream.submissions.append(get_or_create(Submission, submission_id=s.id))
+                submission = get_or_create(Submission, submission_id=s.id)
+                stream.submissions.append(submission)
+                db.session.commit()
+
+
+# move submission_id to submissions if needed
+def move_submissions():
+    q = TwitchStream.query
+    for stream in q:
+        if stream.submission_id:
+            stream.submissions.append(get_or_create(Submission, submission_id=stream.submission_id))
+    db.session.commit()
+
+
+# delete twitch stream's copies
+def delete_copies():
+    q = TwitchStream.query.order_by(TwitchStream.last_time_live.desc().nullslast(), TwitchStream.id)
+    for stream in q.all():
+        copies = q.filter_by(channel=stream.channel).all()
+        if len(copies) > 1:
+            for copy in copies[1:]:
+                stream.submissions += copy.submissions
+                db.session.delete(copy)
 
     db.session.commit()
