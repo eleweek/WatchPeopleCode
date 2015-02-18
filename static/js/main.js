e684c300a39ce7d1eaac9fd55064b995a1388793
Harrison <harrison@theshoebridges.com>
2015-02-18 22:41:23 +1100

    index: youtube stream compatibility

--- static/js/main.js
+++ static/js/main.js
@@ -4,22 +4,29 @@ app.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 			when('/live', {
-				template: "<p>Hey guys</p>"
+				templateUrl: "/templates/stream.html",
+				controller: "LiveCtrl"
 			}).
-			when('upcoming', {
-				template: "<p>I'm upcmomnign</p>"
+			when('/upcoming', {
+				templateUrl: "/templates/stream.html"
 			}).
-			when('completed', {
-				template: "<p>I'm completed</p>"
+			when('/completed', {
+				templateUrl: "/templates/stream.html",
+				controller: "CompletedCtrl"
 			}).
 			otherwise({
 				redirectTo: '/live'
 			});
-	}])
+	}]);
 
-app.controller('MainCtrl', function($scope) {
-	$scope.streams = [
-		{
+app.controller('LiveCtrl', function($scope, $http, $sce) {
+	// $http.get('http://www.watchpeoplecode.com/json').success(function(data) {
+	// 	$scope.streams = process(data, "live")
+
+	// 	console.log($scope.streams)
+	// });
+
+	$scope.streams = process({"live": [{
 	      "title": "Streaming some AngularJS!", 
 	      "url": "http://twitch.tv/paked", 
 	      "user": "paked",
@@ -35,6 +42,70 @@ app.controller('MainCtrl', function($scope) {
 	      "url": "http://twitch.tv/swagcs",
 	      "user": "swagcs", 
 	      "username": "hcwool"
-	    }
-	]
+	    }]}, 'live', $sce)
+
+
 });
+
+app.controller('CompletedCtrl', function($scope, $http, $sce) {
+	console.log("Hello")
+	// $http.get('http://www.watchpeoplecode.com/json').success(function(data) {
+	// 	$scope.streams = process(data, "completed")
+
+	// 	console.log($scope.streams)
+	// });
+
+	$scope.streams = process({"completed": [
+    {
+      "title": "Instruction Set Development - Part 5", 
+      "url": "http://www.youtube.com/watch?v=MCFA-6JPvPQ", 
+      "username": "ConformingCivilian"
+    }, 
+    {
+      "title": "Beginner Java: Lesson 2", 
+      "url": "http://www.youtube.com/watch?v=IT8snRacAcw", 
+      "username": "add7"
+    }, 
+    {
+      "title": "Building a Search Engine -- Season 2 episode 1", 
+      "url": "http://www.youtube.com/watch?v=L2DJhwANoUQ", 
+      "username": "godlikesme"
+    }, 
+    {
+      "title": "Cutting CODE! Ep 3 - Building a SASS to CSS compiler! (well, almost)", 
+      "url": "http://www.youtube.com/watch?v=stF4J3GI_-0", 
+      "username": "davidwhitney"
+    }]}, 'completed', $sce)
+
+});
+
+function process(data, mode, $sce) {
+	streams = data[mode]
+	out = []
+	for (var i = 0; i < streams.length; i++) {
+		stream = streams[i]
+		var l = getLocation(stream.url)
+		stream.youtube = true
+
+		if (l.location == "twitch.tv") {
+			stream.youtube = false
+			url = stream.url.split("/")
+			stream.user = url[url.length - 1]
+		}else {
+			url = stream.url.split("=")
+			stream.id = url[url.length - 1]
+			stream.embed = $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + stream.id + '?html5=1&rel=0');
+		}
+
+		console.log(stream)
+		out.push(stream)
+	}
+
+	return out
+}
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
