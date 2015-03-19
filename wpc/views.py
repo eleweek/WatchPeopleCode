a38e3b9893fc0351f6e4d306226fcc0540cd3aff
Alexander Putilin <avp-13@yandex.ru>
2015-03-19 20:00:33 +0300

    Update rss feed

--- wpc/views.py
+++ wpc/views.py
@@ -192,8 +192,8 @@ def podcast_feed():
     fg.podcast.itunes_category('Technology', 'Podcasting')
     fg.author({'name': 'Nathan Kellert', 'email': 'nathankellert@gmail.com'})
     fg.link(href='http://watchpeoplecode.com/podcast_feed.xml', rel='self')
-    fg.title('WatchPeopleCode')
-    fg.description('WatchPeopleCode(WPC) is a weekly peek into the lives of developers and the WatchPeopleCode community. Our goal is to keep our listeners entertained by giving them new and interesting insights into our industry as well as awesome things happening within our own community. Here, you can expect hear about some of the latest news, tools, and opportunities for developers in nearly every aread of our industry. Most importantly, we hope to have some fun and a few laughs in ways only other nerds know how.')  # NOQA
+    fg.title('WPC Coders Podcast')
+    fg.description('WPC Coders Podcast is a weekly peek into the lives of developers and the WatchPeopleCode community. Our goal is to keep our listeners entertained by giving them new and interesting insights into our industry as well as awesome things happening within our own community. Here, you can expect hear about some of the latest news, tools, and opportunities for developers in nearly every aread of our industry. Most importantly, we hope to have some fun and a few laughs in ways only other nerds know how.')  # NOQA
 
     for epfile, eptitle in [('ep1.mp3', 'Episode 1'), ('ep2.mp3', 'Episode 2'), ('ep3.mp3', 'Episode 3')]:
         epurl = "https://s3.amazonaws.com/wpcpodcast/{}".format(epfile)
