5830c921a084a2f570ff65e9d359044a81c2ef2f
Alexander Putilin <avp-13@yandex.ru>
2015-04-11 01:46:30 +0300

    formatting + fix routing issues in api

--- wpc/api.py
+++ wpc/api.py
@@ -4,17 +4,19 @@ from flask import abort, jsonify
 
 from crossdomain import crossdomain
 
+
 def transform_stream(stream):
     return {
         'id': stream.id,
         'title': stream.title,
-        'user': stream.streamer.reddit_username,
+        'user': stream.streamer.reddit_username if stream.streamer else None,
         'site': stream.type,
         'url': stream.normal_url(),
         'viewers': stream.current_viewers,
         'scheduled_start_time': stream.scheduled_start_time,
         'actual_start_time': stream.actual_start_time
     }
+
 
 def transform_streamer(streamer):
     return {
@@ -34,6 +36,7 @@ def api_streams_live():
         app.logger.exception(e)
         abort(500)
 
+
 @app.route('/api/v1/streams/upcoming')
 @crossdomain(origin='*', max_age=15)
 def api_streams_upcoming():
@@ -43,6 +46,7 @@ def api_streams_upcoming():
     except Exception as e:
         app.logger.exception(e)
         abort(500)
+
 
 @app.route('/api/v1/streams/completed')
 @crossdomain(origin='*', max_age=15)
@@ -54,7 +58,8 @@ def api_streams_past():
         app.logger.exception(e)
         abort(500)
 
-@app.route('/api/v1/streams/<stream_id>')
+
+@app.route('/api/v1/streams/<int:stream_id>')
 @crossdomain(origin='*', max_age=15)
 def api_streams_view(stream_id):
     try:
@@ -76,6 +81,7 @@ def api_streamers():
         app.logger.exception(e)
         abort(500)
 
+
 @app.route('/api/v1/streamers/<name>')
 @crossdomain(origin='*', max_age=15)
 def api_streamers_view(name):
@@ -87,36 +93,39 @@ def api_streamers_view(name):
         app.logger.exception(e)
         abort(500)
 
+
 @app.route('/api/v1/streamers/<name>/upcoming')
 @crossdomain(origin='*', max_age=15)
 def api_streamers_upcoming(name):
     try:
         streamer = Streamer.query.filter_by(reddit_username=name).first_or_404()
-        streams = streamer.streams.filter_by(status = 'upcoming')
+        streams = streamer.streams.filter_by(status='upcoming')
         return jsonify(data=[transform_stream(st) for st in streams],
                        info={'status': 200})
     except Exception as e:
         app.logger.exception(e)
         abort(500)
+
 
 @app.route('/api/v1/streamers/<name>/live')
 @crossdomain(origin='*', max_age=15)
 def api_streamers_live(name):
     try:
         streamer = Streamer.query.filter_by(reddit_username=name).first_or_404()
-        streams = streamer.streams.filter_by(status = 'live')
+        streams = streamer.streams.filter_by(status='live')
         return jsonify(data=[transform_stream(st) for st in streams],
                        info={'status': 200})
     except Exception as e:
         app.logger.exception(e)
         abort(500)
 
+
 @app.route('/api/v1/streamers/<name>/completed')
 @crossdomain(origin='*', max_age=15)
 def api_streamers_past(name):
     try:
         streamer = Streamer.query.filter_by(reddit_username=name).first_or_404()
-        streams = streamer.streams.filter_by(status = 'completed')
+        streams = streamer.streams.filter_by(status='completed')
         return jsonify(data=[transform_stream(st) for st in streams],
                        info={'status': 200})
     except Exception as e:
