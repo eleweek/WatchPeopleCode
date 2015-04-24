c5170257ee10d9508227c44d50ab05bb18839483
Alexander Putilin <avp-13@yandex.ru>
2015-04-25 00:11:35 +0200

    Check if rtmp redirect url is non empty

--- rtmp-stuff/exec_push.sh
+++ rtmp-stuff/exec_push.sh
@@ -2,4 +2,6 @@
 name=$1
 id=$2
 rtmp_url=`curl http://www.watchpeoplecode.com/admin/streamer/$name/rtmp_redirect/$id`
-/usr/bin/avconv -analyzeduration 1000000 -i rtmp://localhost/live/$1 -c:v copy -c:a copy -f flv $rtmp_url
+if [ -z "$rtmp_url" ]; then
+    /usr/bin/avconv -analyzeduration 1000000 -i rtmp://localhost/live/$1 -c:v copy -c:a copy -f flv $rtmp_url
+fi
