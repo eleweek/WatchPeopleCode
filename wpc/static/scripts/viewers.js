76d99743426eaf5da8d2f3dfcd4592bc7dae2272
Alexander Putilin <avp-13@yandex.ru>
2015-05-14 19:04:14 +0300

    Dynamically update viewers

---
+++ wpc/static/scripts/viewers.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var update_viewers = function(stream_id) {
+        console.log("upd viewers");
+        $.ajax("/api/v1/streams/" + stream_id).done(function(data){
+            count = data.data.viewers;
+            console.log(data);
+            console.log(count);
+            cls = "viewers-count-stream-" + stream_id;
+            if (count !== undefined) {
+                $(cls).html(count.toString() + (count === 1 ? "viewer" : "viewers"));
+            }
+        });
+    };
+
+    console.log("ajax viewers");
+    $('.viewers-count-stream').each(function(){
+        var that = this;
+        var stream_id_class = $(that).attr("class").split(' ').filter(
+                function(c) {
+                    return c.indexOf("viewers-count-stream-") === 0;
+                }
+            )[0];
+        var stream_id = +stream_id_class.split('-')[3];
+        console.log("setInterval " + stream_id);
+        setInterval(function() { update_viewers(stream_id); }, 90000);
+    });
+});
