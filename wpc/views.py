9721ceafeaa8f462d372620633b96dacc9edd8e4
Alexander Putilin <avp-13@yandex.ru>
2015-05-31 00:28:10 +0300

    Add the recording of the 3rd episode

--- wpc/views.py
+++ wpc/views.py
@@ -195,11 +195,13 @@ def streamer_page(streamer_name, page):
 
         yt_recording_ep1 = YoutubeStream.query.filter_by(ytid='f968E8eZmvM').one()
         yt_recording_ep2 = YoutubeStream.query.filter_by(ytid='87SfA1sw7vY').one()
+        yt_recording_ep3 = YoutubeStream.query.filter_by(ytid='PHppoe4GxF8').one()
         return render_template('streamers/glm_talkshow.html',
                                streamer=streamer,
                                wpc_stream=wpc_stream,
                                yt_stream_ep1=yt_recording_ep1,
                                yt_stream_ep2=yt_recording_ep2,
+                               yt_stream_ep3=yt_recording_ep3,
                                subscribe_form=subscribe_form)
     elif streamer_name == 'godlikesme':
         yt_stream_the_button = YoutubeStream.query.filter_by(ytid='gNrFy5h2voY').one()
