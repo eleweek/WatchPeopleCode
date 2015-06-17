2d796aafff68fad86583b0ac9c6bc17faf250ca8
Alexander Putilin <avp-13@yandex.ru>
2015-06-17 17:56:26 +0200

    use ffmpeg instead of avconv for cross-streaming

--- rtmp-stuff/exec_push.sh
+++ rtmp-stuff/exec_push.sh
@@ -2,6 +2,8 @@
 name=$1
 id=$2
 rtmp_url=`curl http://www.watchpeoplecode.com/admin/streamer/$name/rtmp_redirect/$id`
-if [ ! -z "$rtmp_url" ]; then
-    /usr/bin/avconv -analyzeduration 1000000 -i rtmp://localhost/live/$1 -c:v copy -c:a copy -f flv $rtmp_url
+echo $name, $id, $rtmp_url
+if [ ! -z "$rtmp_url" ]; then 
+    # /usr/bin/avconv -analyzeduration 1000000 -i rtmp://localhost/live/$1 -c:v copy -c:a copy -f flv $rtmp_url
+    ffmpeg -i rtmp://localhost/live/$1 -vcodec copy -acodec copy -f flv $rtmp_url
 fi
