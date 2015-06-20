c5f4ba7dadd68541531e7638d80294ffd1876baf
elsie <elimohl@gmail.com>
2015-06-20 20:32:52 +0300

    allow unicode in truncated message

--- wpc/views.py
+++ wpc/views.py
@@ -508,7 +508,7 @@ def chat_message(message_text, streamer_username):
     streamer = check_chat_access_and_get_streamer(streamer_username)
     # TODO: this is quickfix
     if len(message_text) > 2048:
-        message_text = "{}...".format(message_text[:2045])
+        message_text = u"{}...".format(message_text[:2045])
     message = {"sender": session['username'],
                "text": nl2br_py(message_text)}
     if current_user.is_anonymous() and\
