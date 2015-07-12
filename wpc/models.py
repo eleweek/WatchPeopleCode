381a60d0e923f5dd5a25a760812044c451b273ed
elsie <elimohl@gmail.com>
2015-07-13 01:58:02 +0300

    fix bug in updating yc for ys

--- wpc/models.py
+++ wpc/models.py
@@ -244,7 +244,7 @@ class YoutubeStream(Stream):
             elif self.streamer is not None:
                 # if streamer has no yc and didn't ever checked profile
                 if not self.streamer.checked:
-                    self.streamer.yc = self.youtube_channel
+                    self.streamer.youtube_channel_class = self.youtube_channel
                 # otherwise
                 else:
                     self.streamer = None
