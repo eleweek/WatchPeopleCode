c248ce82cc233641b99c67afee2a4e00482c69b8
Alexander Putilin <avp-13@yandex.ru>
2015-07-20 23:50:47 +0300

    Less annoying loging in js

--- wpc/static/scripts/viewers.js
+++ wpc/static/scripts/viewers.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
     var update_viewers = function(stream_id) {
         $.ajax("/api/v1/streams/" + stream_id).done(function(data){
             count = data.data.viewers;
-            console.log(data);
-            console.log(count);
             cls_selector = ".viewers-count-stream-" + stream_id;
             if (count !== undefined) {
                 $(cls_selector).html(count.toString() + " " + (count === 1 ? "viewer" : "viewers"));
@@ -19,7 +17,6 @@ $(document).ready(function() {
                 }
             )[0];
         var stream_id = +stream_id_class.split('-')[3];
-        console.log("setInterval " + stream_id);
         setInterval(function() { update_viewers(stream_id); }, 20000);
     });
 });
